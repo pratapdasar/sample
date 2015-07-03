5d474789a2da6cc5d96a4ec0af4e1de79593a2e3
testuser <testuser@ebe34124-db29-449a-902f-ab04737c7a2e>
2015-07-03 07:25:16 +0000

    initial import

---
+++ trunk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleSvn::Application.config.secret_token = '9af303ab55681ca1c8a9e0c5bf6486d37a962fa5bd8abed7c94ebfbaca5ec687b274ee4c5dfbbb274fa5f01ebaa4a0ce83d414722f6c62c018c44422ac9edf2d'
